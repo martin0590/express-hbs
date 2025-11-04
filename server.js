3ce31016099fd607b785a439bd4492f75165396c
Martin M <martin.ale590@gmail.com>
2025-11-04 17:39:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+
+const app = express();
+
+const friends = [
+  {
+    id: 0,
+    name: "Albert Einstein",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body?.name) {
+    return res.status(400).json({
+      error: "Missing friend name",
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+
+  friends.push(newFriend);
+  res.json(newFriend);
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});

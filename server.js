e9dc914f2e2ce132a93de414bd826e780dec2aeb
Martin M <martin.ale590@gmail.com>
2025-11-06 15:41:58 -0300

    added - routes

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 const express = require("express");
 
-const messagesController = require('./controllers/messages.controller')
-const friendsController = require('./controllers/friends.controller')
+const friendsRouter = require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router')
 
 const app = express();
-
-
-
-
 
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post("/friends", friendsController.postFriend);
-app.get("/friends", friendsController.getFriends);
-app.get("/friends/:friendId", friendsController.getFriend);
-
-app.get('/messages', messagesController.getMessages)
-app.post('/messages',messagesController.postMessage)
+app.use('/friends',friendsRouter) //sometimes called mounting the friends router on the app object
+app.use('/messages',messagesRouter)
 
 app.listen(3000, () => {
   console.log("server running on port 3000");

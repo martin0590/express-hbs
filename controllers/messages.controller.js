6eed03ecf7552c56065f178e7a5a004584350eb4
Martin M <martin.ale590@gmail.com>
2025-11-06 19:22:43 -0300

    added - hbs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,8 +2,11 @@ const path = require('path')
 
 // function instead of arrow function will appear if an error occurs
 function getMessages(req, res) {
-  // res.send("<ul><li>Hello Albert Einstein!<li><ul>");
-  res.sendFile(path.join(__dirname, '..', 'public', 'images' ,'skymountain.jpg'))
+  res.render('messages', {
+    title: 'messages to my friends',
+    friend: 'bill gates'
+  })
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images' ,'skymountain.jpg'))
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express");
 const path = require('path')
+const express = require("express");
 
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
 
 const app = express();
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -13,8 +15,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-app.use('/site',express.static(path.join(__dirname, 'public'))) //static is relative to where i started my node application
+app.use('/',express.static(path.join(__dirname, 'public'))) //static is relative to where i started my node application
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'The Mountains',
+    caption: "The Mountains and the Sky"
+  })
+})
 
 app.use('/friends',friendsRouter) //sometimes called mounting the friends router on the app object
 app.use('/messages',messagesRouter)

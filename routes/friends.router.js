c515d4e51ae72e44c2606f4344802973906acea1
Martin M <martin.ale590@gmail.com>
2025-11-06 19:28:18 -0300

    chore - fixed route

--- routes/friends.router.js
+++ routes/friends.router.js
@@ -10,7 +10,7 @@ friendsRouter.use((req, res, next) => {
 })
 
 friendsRouter.post("/", friendsController.postFriend);
-friendsRouter.get("", friendsController.getFriends);
+friendsRouter.get("/", friendsController.getFriends);
 friendsRouter.get("/:friendId", friendsController.getFriend);
 
 module.exports = friendsRouter

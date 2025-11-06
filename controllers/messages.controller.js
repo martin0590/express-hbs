9e128090981a60cfdc3b2de42f841b95414c43f8
Martin M <martin.ale590@gmail.com>
2025-11-06 18:32:22 -0300

    added - static files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 // function instead of arrow function will appear if an error occurs
 function getMessages(req, res) {
-  res.send("<ul><li>Hello Albert Einstein!<li><ul>");
+  // res.send("<ul><li>Hello Albert Einstein!<li><ul>");
+  res.sendFile(path.join(__dirname, '..', 'public', 'images' ,'skymountain.jpg'))
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path')
 
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site',express.static(path.join(__dirname, 'public'))) //static is relative to where i started my node application
 app.use(express.json());
 
 app.use('/friends',friendsRouter) //sometimes called mounting the friends router on the app object
